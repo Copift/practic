de4a4b5533b61b395121b86cea2c708407e16233
copift <copift@yandex.ru>
2023-05-06 16:39:56 +0800

    add insert data(test) in table 1.

--- Builder.py
+++ Builder.py
@@ -1,46 +1,75 @@
 import docx
 
 from Settings import Settings
-from PyPDF2 import PdfReader
+
 from docx import Document
+from docx.enum.table import WD_CELL_VERTICAL_ALIGNMENT
+from docx.enum.table import WD_TABLE_ALIGNMENT
 class Builder():
 
-    def __init__(self,insertData,settings:Settings) -> None:
-     self.insertData=insertData
-     self.settings=settings
-     self.PathToDeploy=settings.ini['PATHS']["Deploy"]
-     self.PathToSh=settings.ini["PATHS"]["sh"]
-     self.out =Document(self.PathToSh)
-     self.tags={}
+    def __init__(self, insertData, settings: Settings) -> None:
+        self.insertData = insertData
+        self.settings = settings
+        self.PathToDeploy = settings.ini['PATHS']["Deploy"]
+        self.PathToSh = settings.ini["PATHS"]["sh"]
+        self.out = Document(self.PathToSh)
+        self.tags = {}
+
     def getTags(self):
-     fullText = []
-     for para in self.out.paragraphs:
-      fullText.append(para.text)
-     self.tags={}
-     for text in fullText:
-      if "{" in text and "}" in text:
-       self.tags.update({text.split('{')[1].split('}')[0]:None})
+        fullText = []
+        for para in self.out.paragraphs:
+            fullText.append(para.text)
+        self.tags = {}
+        for text in fullText:
+            if "{" in text and "}" in text:
+                self.tags.update({text.split('{')[1].split('}')[0]: None})
+
     def checkInputData(self):
 
-     for tag in self.tags.keys():
-      if not(tag in self.insertData.keys()):
-       raise Exception(f"{tag} not finded")
-     for tag in self.insertData.keys():
-      if not (tag in self.tags.keys()):
-       raise Warning(f"{tag} not used in document")
+        for tag in self.tags.keys():
+            if not (tag in self.insertData.keys()):
+                raise Exception(f"{tag} not finded")
+        for tag in self.insertData.keys():
+            if not (tag in self.tags.keys()):
+                print( Warning(f"{tag} not used in document"))
 
     def build(self):
-     self.getTags()
-     self.checkInputData()
-     for para in self.out.paragraphs:
-       text=para.text
-       if "{" in text and "}" in text:
-        for key in self.insertData.keys():
-         if key in text:
-          para.text=para.text.replace(f"{'{'}{key}{'}'}",self.insertData[key])
-     self.out.save(self.PathToDeploy+"/Deploy.docx")
+        self.getTags()
+        self.checkInputData()
+        self.replaceTags()
+        self.createTable1_1(self.insertData["table1_1"])
+        self.out.save(self.PathToDeploy + "/Deploy.docx")
+    def replaceTags(self):
+        for para in self.out.paragraphs:
+            text = para.text
+            if "{" in text and "}" in text:
+                for key in self.insertData.keys():
+                    if key in text:
+                        para.text = para.text.replace(f"{'{'}{key}{'}'}", self.insertData[key])
 
+    def createRowForTable1_1(self,table,t):
+        lenRows = len(table.rows)
+        for n in range(t):
+            table.add_row()
+        for n in range(3):
+            for i in range(t):
+                table.cell(row_idx=lenRows + i, col_idx=n).merge(table.cell(row_idx=lenRows + n, col_idx=n))
+        return table
+    def createTable1_1(self,table1_1):
+            table=self.out.tables[0]
+            ind=1
+            for RPD in table1_1:
+                lenRows = len(table.rows)
+                table=self.createRowForTable1_1(table,len(RPD["indexes"]))
+                table.cell(row_idx=lenRows+2,col_idx=0).text=str(ind)
+                table.cell(row_idx=lenRows + 2, col_idx=1).text = str(RPD['codeRPDs'])
+                table.cell(row_idx=lenRows + 2, col_idx=2).text = str(RPD['soder_komp'])
+                i=0
+                for index in RPD["indexes"]:
+                    table.cell(row_idx=lenRows + i, col_idx=3).text = str(index["index"])
+                    table.cell(row_idx=lenRows + i, col_idx=4).text = str(index["rez"])
+                    table.cell(row_idx=lenRows + i, col_idx=5).text = str(index["name"])
+                    i+=1
+                ind+=1
 
-
-
-
+            table.style = 'Table Grid'
--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from Builder import Builder
 from Settings import Settings
 
 settings=Settings()
-print(settings.ini)
-insertData={'input.inst': "ИГУ", 'upload.department': "ФБКИ", 'input.discipline': "Прикладная информатика", 'input.dirPod': "разработка ПО", 'input.naprav': "ПИ", 'input.developers': "Сергеев Д.В", 'upload.OS_RPD': "Список"}
+insertData={'fig':'dsds','input.inst': "ИГУ", 'upload.department': "ФБКИ", 'input.discipline': "Прикладная информатика", 'input.dirPod': "разработка ПО", 'input.naprav': "ПИ", 'input.developers': "Сергеев Д.В", 'upload.OS_RPD': "Список"}
+insertTable1_1=[]
+indexes = []
+for i in range(3,6):
+    indexes = []
+    for t in range(i):
+        indexes.append({"index":f'{i-2}_{t}',"rez":" test test test test test test test test test test ","name":"test test test test test test test test test test "})
+    insertTable1_1.append({"codeRPDs":i+2324234234,"soder_komp":f"soder_{i-2}","indexes":indexes})
+insertData.update({'table1_1':insertTable1_1})
 build=Builder(insertData=insertData,settings=settings)
-build.build()
+print(insertTable1_1)
+build.build(
+)
